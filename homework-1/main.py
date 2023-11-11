aa3efc1a5ea513a37a32b6a8cb6714c15c41ae95
Aleksandr Podcherednik <startcpa1@yandex.ru>
2023-11-11 13:38:49 +0300

    add fixes

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,9 +1,9 @@
 from utils import fill_table_employees, fill_table_customers, fill_table_orders, get_data_from_csv
 
-data = get_data_from_csv('employees_data.csv')
-data1 = get_data_from_csv('customers_data.csv')
-data2 = get_data_from_csv('orders_data.csv')
+data_employees = get_data_from_csv('employees_data.csv')
+data_customers = get_data_from_csv('customers_data.csv')
+data_orders = get_data_from_csv('orders_data.csv')
 
-fill_table_employees(data, 'employees')
-fill_table_customers(data1, 'customers')
-fill_table_orders(data2, 'orders')
+fill_table_employees(data_employees)
+fill_table_customers(data_customers)
+fill_table_orders(data_orders)
--- homework-1/utils.py
+++ homework-1/utils.py
@@ -14,34 +14,34 @@ def get_data_from_csv(filename):
         return list(data)
 
 
-def fill_table_employees(data, table_name):
+def fill_table_employees(data):
     """Записываем в таблицу employees данные из списка словарей """
     conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='12345')
     cur = conn.cursor()
     for el in data:
         cur.execute(
-            f"INSERT INTO {table_name} VALUES (%s, %s, %s, %s, %s, %s);",
+            "INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s);",
             (el['employee_id'], el['first_name'], el['last_name'], el['title'], el['birth_date'], el['notes']))
         conn.commit()
 
 
-def fill_table_customers(data, table_name):
+def fill_table_customers(data):
     """Записываем в таблицу customers данные из списка словарей """
     conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='12345')
     cur = conn.cursor()
     for el in data:
         cur.execute(
-            f"INSERT INTO {table_name} VALUES (%s, %s, %s);",
+            "INSERT INTO customers VALUES (%s, %s, %s);",
             (el['customer_id'], el['company_name'], el['contact_name']))
         conn.commit()
 
 
-def fill_table_orders(data, table_name):
+def fill_table_orders(data):
     """Записываем в таблицу orders данные из списка словарей """
     conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='12345')
     cur = conn.cursor()
     for el in data:
         cur.execute(
-            f"INSERT INTO {table_name} VALUES (%s, %s, %s, %s, %s);",
+            "INSERT INTO orders VALUES (%s, %s, %s, %s, %s);",
             (el['order_id'], el['customer_id'], el['employee_id'], el['order_date'], el['ship_city']))
         conn.commit()
