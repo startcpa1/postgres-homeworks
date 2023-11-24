7b91f78e68b797ee9b9ac328db2537d090af1266
Aleksandr Podcherednik <startcpa1@yandex.ru>
2023-11-24 22:34:00 +0300

    added sql queries

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,32 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
-
+CREATE TABLE student (
+  student_id SERIAL PRIMARY KEY,
+  first_name VARCHAR,
+  last_name VARCHAR,
+  birthday DATE,
+  phone VARCHAR
+);
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student
+ADD COLUMN middle_name VARCHAR;
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student
+DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student
+RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student
+ALTER COLUMN phone TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO students (name, age) VALUES
+('Ivan', 25),
+('Petr', 30),
+('Evgeniy', 28);
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE students;
